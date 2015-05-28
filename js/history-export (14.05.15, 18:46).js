edb2a3e700349649484527b9c946606437b3577c
Vladimir Kunin <v.b.kunin@gmail.com>
2015-05-28 18:13:07 +0300

    Delete history-export (14.05.15, 18:46).js

--- js/history-export (14.05.15, 18:46).js
+++
@@ -1,7 +0,0 @@
-function HistoryExport(sFilter)
-{
-  var sUrl = GetAbsoluteUrlModulesRoot()+'history-exporter/export.php';
-  $.post(sUrl, {filter: sFilter}, function(data) {
-    $('body').append(data);
-  });
-}
